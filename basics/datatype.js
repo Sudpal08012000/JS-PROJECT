34f4b33a9b5dcab6ad739528f202399af34b8279
Sudhanshu <98095981+Sudpal08012000@users.noreply.github.com>
2023-11-16 14:15:44 +0000

    dataTypes

---
+++ basics/datatype.js
@@ -0,0 +1,19 @@
+   "use strict";  // traet all JS code as newer version
+
+  // alert("sudhu") we are using nodejs, not browser
+
+  //never write two line in same or one line write in two diffrent line code readability shoud be high
+
+  // number => 2 to power 53
+  //bigint
+  // string=>""
+  //boolean=> true/false
+  // null => standalone value
+  // undefined => 
+  // symbol => unique  //use when we use react or figma//
+  
+
+  console.log(typeof age);// use to identify the type it is string,bolean , number etc//
+
+  console.log(typeof null);// object
+  console.log(typeof undefined); // undefined
